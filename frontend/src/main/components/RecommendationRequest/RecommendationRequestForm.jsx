6fd87515d6fba83d836f08b89df53c6ab1a464a4
Hrithik Mereddy <hrithikmereddy@ucsb.edu>
2026-04-26 14:55:10 -0700

    Fixed ESlint

--- frontend/src/main/components/RecommendationRequest/RecommendationRequestForm.jsx
+++ frontend/src/main/components/RecommendationRequest/RecommendationRequestForm.jsx
@@ -2,7 +2,9 @@ import { Button, Form, Row, Col } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const email_regex = /\S+@\S+\.\S+/;
+// eslint-disable-next-line react-refresh/only-export-components
 export const isodate_regex =
   /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
 
