03af450815f4353992e4f298c0e2cdbfcb973821
Hrithik Mereddy <hrithikmereddy@ucsb.edu>
2026-04-26 20:46:29 -0700

    Fixed formatting

--- frontend/src/stories/pages/RecommendationRequest/RecommendationRequestEditPage.stories.jsx
+++ frontend/src/stories/pages/RecommendationRequest/RecommendationRequestEditPage.stories.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import { recommendationRequestFixtures } from "fixtures/recommendationRequestFixtures";
@@ -7,8 +7,8 @@ import { http, HttpResponse } from "msw";
 import RecommendationRequestEditPage from "main/pages/RecommendationRequest/RecommendationRequestEditPage";
 
 export default {
-    title: 'pages/RecommendationRequest/RecommendationRequestEditPage',
-    component: RecommendationRequestEditPage
+  title: "pages/RecommendationRequest/RecommendationRequestEditPage",
+  component: RecommendationRequestEditPage,
 };
 
 const Template = () => <RecommendationRequestEditPage />;
@@ -16,19 +16,21 @@ const Template = () => <RecommendationRequestEditPage />;
 export const Default = Template.bind({});
 
 Default.parameters = {
-    msw: [
-        http.get('/api/currentUser', () => {
-            return HttpResponse.json(apiCurrentUserFixtures.userOnly);
-        }),
-        http.get('/api/systemInfo', () => {
-            return HttpResponse.json(systemInfoFixtures.showingNeither);
-        }),
-        http.get('/api/recommendationrequest', () => {
-            return HttpResponse.json(recommendationRequestFixtures.threeRecommendationRequests[0]);
-        }),
-        http.put('/api/recommendationrequest', async ({ request }) => {
-            const data = await request.json();
-            return HttpResponse.json({ id: 17, ...data });
-        }),
-    ],
-}
+  msw: [
+    http.get("/api/currentUser", () => {
+      return HttpResponse.json(apiCurrentUserFixtures.userOnly);
+    }),
+    http.get("/api/systemInfo", () => {
+      return HttpResponse.json(systemInfoFixtures.showingNeither);
+    }),
+    http.get("/api/recommendationrequest", () => {
+      return HttpResponse.json(
+        recommendationRequestFixtures.threeRecommendationRequests[0],
+      );
+    }),
+    http.put("/api/recommendationrequest", async ({ request }) => {
+      const data = await request.json();
+      return HttpResponse.json({ id: 17, ...data });
+    }),
+  ],
+};
