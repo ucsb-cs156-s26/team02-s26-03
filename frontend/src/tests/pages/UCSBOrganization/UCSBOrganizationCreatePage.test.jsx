654b9cfa699e05a42d6bd0e9787204e162632656
Raymond Xie <rxie516@ucsb.edu>
2026-05-03 19:31:18 -0700

    Add UCSBOrganization create page

--- frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.jsx
+++ frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import UCSBOrganizationCreatePage from "main/pages/UCSBOrganization/UCSBOrganizationCreatePage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MemoryRouter } from "react-router";
@@ -7,7 +7,27 @@ import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-import { expect } from "vitest";
+
+const mockToast = vi.fn();
+vi.mock("react-toastify", async (importOriginal) => {
+  const originalModule = await importOriginal();
+  return {
+    ...originalModule,
+    toast: vi.fn((x) => mockToast(x)),
+  };
+});
+
+const mockNavigate = vi.fn();
+vi.mock("react-router", async (importOriginal) => {
+  const originalModule = await importOriginal();
+  return {
+    ...originalModule,
+    Navigate: vi.fn((x) => {
+      mockNavigate(x);
+      return null;
+    }),
+  };
+});
 
 describe("UCSBOrganizationCreatePage tests", () => {
   const axiosMock = new AxiosMockAdapter(axios);
@@ -23,9 +43,39 @@ describe("UCSBOrganizationCreatePage tests", () => {
       .reply(200, systemInfoFixtures.showingNeither);
   };
 
-  const queryClient = new QueryClient();
-  test("Renders expected content", async () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
     setupUserOnly();
+  });
+
+  test("renders without crashing", async () => {
+    const queryClient = new QueryClient();
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <UCSBOrganizationCreatePage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+
+    expect(
+      await screen.findByText("Create New UCSBOrganization"),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByTestId("UCSBOrganizationForm-orgCode"),
+    ).toBeInTheDocument();
+  });
+
+  test("on submit, makes request to backend, and redirects to /ucsborganization", async () => {
+    const queryClient = new QueryClient();
+    const ucsbOrganization = {
+      orgCode: "LIBR",
+      orgTranslationShort: "Library",
+      orgTranslation: "UCSB Library",
+      inactive: true,
+    };
+
+    axiosMock.onPost("/api/UCSBOrganization/post").reply(202, ucsbOrganization);
 
     render(
       <QueryClientProvider client={queryClient}>
@@ -35,9 +85,63 @@ describe("UCSBOrganizationCreatePage tests", () => {
       </QueryClientProvider>,
     );
 
-    await screen.findByText("UCSBOrganization Create page not yet implemented");
+    await screen.findByTestId("UCSBOrganizationForm-orgCode");
+
+    fireEvent.change(screen.getByTestId("UCSBOrganizationForm-orgCode"), {
+      target: { value: "LIBR" },
+    });
+    fireEvent.change(
+      screen.getByTestId("UCSBOrganizationForm-orgTranslationShort"),
+      {
+        target: { value: "Library" },
+      },
+    );
+    fireEvent.change(
+      screen.getByTestId("UCSBOrganizationForm-orgTranslation"),
+      {
+        target: { value: "UCSB Library" },
+      },
+    );
+    fireEvent.click(screen.getByTestId("UCSBOrganizationForm-inactive"));
+    fireEvent.click(screen.getByTestId("UCSBOrganizationForm-submit"));
+
+    await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+    expect(axiosMock.history.post[0].params).toEqual({
+      orgCode: "LIBR",
+      orgTranslationShort: "Library",
+      orgTranslation: "UCSB Library",
+      inactive: true,
+    });
+
+    expect(mockToast).toBeCalledWith(
+      "New UCSBOrganization Created - orgCode: LIBR",
+    );
+    expect(mockNavigate).toBeCalledWith({ to: "/ucsborganization" });
+  });
+
+  test("on invalid submit, shows errors and does not call backend", async () => {
+    const queryClient = new QueryClient();
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <UCSBOrganizationCreatePage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+
+    fireEvent.click(await screen.findByTestId("UCSBOrganizationForm-submit"));
+
     expect(
-      screen.getByText("UCSBOrganization Create page not yet implemented"),
+      await screen.findByText("Org Code is required."),
     ).toBeInTheDocument();
+    expect(
+      screen.getByText("Org Translation Short is required."),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Org Translation is required."),
+    ).toBeInTheDocument();
+    expect(axiosMock.history.post.length).toBe(0);
   });
 });
